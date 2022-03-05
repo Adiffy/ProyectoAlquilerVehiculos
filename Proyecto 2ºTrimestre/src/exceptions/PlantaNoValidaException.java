a44519ca347d6d5589a0c7d0420d026a2764d189
Víctor Esquinas <93326902+Adiffy@users.noreply.github.com>
2022-03-05 15:16:52 +0100

    Add files via upload

---
+++ Proyecto 2ºTrimestre/src/exceptions/PlantaNoValidaException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+@SuppressWarnings("serial")
+public class PlantaNoValidaException extends Exception {
+
+	
+	public PlantaNoValidaException(String message) {
+		super(message);
+		
+	}
+
+
+
+}
