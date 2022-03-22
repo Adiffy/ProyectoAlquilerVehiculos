4e5cd1cc4ef10c1f930f57c77ebf9e6eec0b82ae
Víctor Esquinas <93326902+Adiffy@users.noreply.github.com>
2022-03-22 10:03:27 +0100

    1.9
    Primer acercamiento a leer de una base de datos

---
+++ Proyecto 2ºTrimestre/src/accesoADatos/EmpresaDB.java
@@ -0,0 +1,24 @@
+package accesoADatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class EmpresaDB {
+
+	//Propiedades estáticas
+	static String cadenaConexion = "jdbc:oracle:thin:@localhost:1521/Alquileres";
+	static Connection conn;
+	
+	/**
+	 * Método que crea una conexión con la base de datos
+	 */
+	public static void abreConexion()
+	{
+		try {
+			conn = DriverManager.getConnection(cadenaConexion, "Victor", "1234");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
